9d4eeaaf376177fa2dc86e60bb86ce56a39f8b66
綦帅鹏 <qishuaipeng@qishuaipengdeiMac.local>
2019-04-18 17:40:24 +0800

    初始化

---
+++ DateTimeExamples/DateTimeExamples/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DateTimeExamples
+//
+//  Created by 綦帅鹏 on 2019/4/18.
+//  Copyright © 2019年 QSP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DateTimeExamples/DateTimeExamples/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DateTimeExamples
+//
+//  Created by 綦帅鹏 on 2019/4/18.
+//  Copyright © 2019年 QSP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
